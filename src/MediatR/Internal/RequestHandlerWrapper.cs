06044c775ee58697d94149cfd96140f7f117abe9
Shengjie Yan <ysjshengjie@live.cn>
2019-01-16 13:53:31 +0800

    添加注释解读处理管道的构建

--- src/MediatR/Internal/RequestHandlerWrapper.cs
+++ src/MediatR/Internal/RequestHandlerWrapper.cs
@@ -17,12 +17,15 @@ namespace MediatR.Internal
             }
             catch (Exception e)
             {
-                throw new InvalidOperationException($"Error constructing handler for request of type {typeof(THandler)}. Register your handlers with the container. See the samples in GitHub for examples.", e);
+                throw new InvalidOperationException(
+                    $"Error constructing handler for request of type {typeof(THandler)}. Register your handlers with the container. See the samples in GitHub for examples.",
+                    e);
             }
 
             if (handler == null)
             {
-                throw new InvalidOperationException($"Handler was not found for request of type {typeof(THandler)}. Register your handlers with the container. See the samples in GitHub for examples.");
+                throw new InvalidOperationException(
+                    $"Handler was not found for request of type {typeof(THandler)}. Register your handlers with the container. See the samples in GitHub for examples.");
             }
 
             return handler;
@@ -38,15 +41,48 @@ namespace MediatR.Internal
     internal class RequestHandlerWrapperImpl<TRequest, TResponse> : RequestHandlerWrapper<TResponse>
         where TRequest : IRequest<TResponse>
     {
+
         public override Task<TResponse> Handle(IRequest<TResponse> request, CancellationToken cancellationToken,
             ServiceFactory serviceFactory)
         {
-            Task<TResponse> Handler() => GetHandler<IRequestHandler<TRequest, TResponse>>(serviceFactory).Handle((TRequest) request, cancellationToken);
+            //局部函数&&表达式形式的成员函数
+            //这一步的目的，是将一个类型函数对象转换为局部函数，方便后续调用。
+            //Task<TResponse> Handler() => GetHandler<IRequestHandler<TRequest, TResponse>>(serviceFactory).Handle((TRequest)request, cancellationToken);
+            //相当于下面的写法：
+            Task<TResponse> Handler()
+            {
+                return GetHandler<IRequestHandler<TRequest, TResponse>>(serviceFactory) //从IOC容器获取该请求对应的请求处理器
+                    .Handle((TRequest) request, cancellationToken); //调用Handle方法
+            }
+
+            //将函数调用转为委托
+            RequestHandlerDelegate<TResponse> handler = () =>
+                GetHandler<IRequestHandler<TRequest, TResponse>>(serviceFactory)
+                    .Handle((TRequest) request, cancellationToken);
+
+            Func<RequestHandlerDelegate<TResponse>, IPipelineBehavior<TRequest, TResponse>,
+                RequestHandlerDelegate<TResponse>> combineFunc = (delegater, behavior) =>
+            {
+                //将类型函数对象转换为委托
+                RequestHandlerDelegate<TResponse> combineDelegate =
+                    () => behavior.Handle((TRequest) request, cancellationToken, delegater);
+                //相当于下面的写法
+                //RequestHandlerDelegate<TResponse> combineDelegate = delegate { return behavior.Handle((TRequest)request, cancellationToken, delegater); };
+
+                return combineDelegate;
+            };
+
 
             return serviceFactory
-                .GetInstances<IPipelineBehavior<TRequest, TResponse>>()
-                .Reverse()
-                .Aggregate((RequestHandlerDelegate<TResponse>) Handler, (next, pipeline) => () => pipeline.Handle((TRequest)request, cancellationToken, next))();
+                .GetInstances<IPipelineBehavior<TRequest, TResponse>>() //从IOC容器中获取所有的IPipelineBehavior的实现，用于构造请求管道
+                .Reverse() //集合反转
+                .Aggregate(
+                    (RequestHandlerDelegate<TResponse>) Handler, //将局部函数强制转换为委托，等价于handler
+                    (next, pipeline) =>
+                        () => pipeline.Handle((TRequest) request, cancellationToken,
+                            next) ////这一步是借助委托构造函数链，返回的是一个委托，等价于combineDelegate
+                )//累加操作返回的是一个由委托构造而成的函数链
+                (); //加上()进行委托调用
         }
     }
 }
